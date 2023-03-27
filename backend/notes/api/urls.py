dd1c0f3cc53bded231f3849981c38ba82b1f9a87
Omar Reda <omar90180360@gmail.com>
2023-03-27 04:22:22 +0200

    APIs, Permissions and JWT

---
+++ backend/notes/api/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+
+from .views import get_routes, MyTokenObtainPairView
+from ..views import (
+    Notes,
+    NoteCreate,
+    NoteGetUpdateDelete
+)
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+app_name = 'api'
+
+
+urlpatterns = [
+    path('', get_routes),
+    path('notes/', Notes.as_view()),
+    path('create/', NoteCreate.as_view()),
+    path('note/<int:id>', NoteGetUpdateDelete.as_view(), name='my_note'),
+
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
